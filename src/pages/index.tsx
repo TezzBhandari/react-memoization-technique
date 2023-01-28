497e47f451be7dfbe8548bba3c00dfeafacacb5d
TezzBhandari <bhandaritezz18@gmail.com>
2023-01-29 02:32:30 +0800

    useMemo: Used for referential object | replace the memo's areEquals parameter

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
-import { useState, memo } from "react";
+import { useState, memo, useMemo } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export type SwatchProps = {
-  color: string;
+  params: { color: string };
 };
 
-const Swatch: React.FC<SwatchProps> = ({ color }) => {
-  console.log(`Swatch Rendered with ${color}`);
+const Swatch: React.FC<SwatchProps> = ({ params }) => {
+  console.log(`Swatch Rendered with ${params.color}`);
   return (
-    <div className={`w-16 h-16 m-4`} style={{ backgroundColor: color }}></div>
+    <div
+      className={`w-16 h-16 m-4`}
+      style={{ backgroundColor: params.color }}
+    ></div>
   );
 };
 
@@ -19,13 +22,35 @@ const Swatch: React.FC<SwatchProps> = ({ color }) => {
  * memo is a higher order function
  * It creates component i.e, memoized component
  * The memoized component only re-renders if it's props changes
+ * only works for primitive types unles you specify areEquals parameter
+ * Use useMemo for object types
  */
-const MemoedSwatch = memo(Swatch);
+const MemoedSwatch = memo(
+  Swatch
+  // (
+  //   prevProps: Readonly<SwatchProps>,
+  //   nextProps: Readonly<SwatchProps>
+  // ): boolean => {
+  //   return prevProps.params.color === nextProps.params.color;
+  // }
+);
 
 export default function Home() {
   const [appRenderIndex, setAppRenderIndex] = useState(0);
   const [color, setColor] = useState("red");
   console.log(`App Rendered ${appRenderIndex}`);
+
+  /**
+   * useMemo return the same object if the value in the dependency array does not changes even with the re-render
+   * it will return the object with the same address i.e exact same object reference, if the dependency value does not change
+   */
+  const params = useMemo(
+    () => ({
+      color,
+    }),
+    [color]
+  );
+
   return (
     <>
       <Head>
@@ -51,8 +76,9 @@ export default function Home() {
           </button>
         </div>
         <div>
-          <MemoedSwatch color={color} />
-          <MemoedSwatch color={color == "red" ? "blue" : "red"} />
+          {/* Everytime it re-renders, it creates new object with new reference. It may have excatly the same value but it always has new reference */}
+          <MemoedSwatch params={params} />
+          {/* <MemoedSwatch color={color == "red" ? "blue" : "red"} /> */}
         </div>
       </div>
     </>
